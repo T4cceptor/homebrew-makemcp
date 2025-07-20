3b5df76b71ff63087a9f327335e5c025d42f71e5
Benedikt Reschberger <github@squaapi.com>
2025-07-20 14:16:26 +0200

    Added correct sha256 values

--- Formula/makemcp.rb
+++ Formula/makemcp.rb
@@ -8,22 +8,22 @@ class Makemcp < Formula
   on_macos do
     on_intel do
       url "https://github.com/T4cceptor/MakeMCP/releases/download/v0.2.1/makemcp-v0.2.1-darwin-amd64.tar.gz"
-      sha256 "PLACEHOLDER_INTEL_SHA256"
+      sha256 "875db09126837900433d5e81066c1d2f59e8a86373ab750b8bc8b27e9e041514"
     end
     on_arm do
       url "https://github.com/T4cceptor/MakeMCP/releases/download/v0.2.1/makemcp-v0.2.1-darwin-arm64.tar.gz"
-      sha256 "PLACEHOLDER_ARM64_SHA256"
+      sha256 "36606552f9f1369a218cda4b336f333a870c7feb6c6a96c0b13338f3100c425a"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/T4cceptor/MakeMCP/releases/download/v0.2.1/makemcp-v0.2.1-linux-amd64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_INTEL_SHA256"
+      sha256 "579cabbb9311f84c611a8b7813b34bfc5f76fe32ccf6952c86056c841195ad35"
     end
     on_arm do
       url "https://github.com/T4cceptor/MakeMCP/releases/download/v0.2.1/makemcp-v0.2.1-linux-arm64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_ARM64_SHA256"
+      sha256 "73f6dd03547aee09b97687fba396be2a0a58a06bc79e6f6d011e593ffe4a422a"
     end
   end
 
